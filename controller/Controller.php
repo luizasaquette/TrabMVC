594aa9c2455b5673f1871e8cbc62ba9a0b2ef7c0
aluno <aluno@PC504>
2019-12-18 09:06:50 -0200

    oi, arrumei

--- controller/Controller.php
+++ controller/Controller.php
@@ -25,6 +25,9 @@ class Controller {
             case 'logout':
                 $this->showTelaDeLogoutView();
                 break;
+            case 'luiza':
+                $this->showTelaLuiza();
+            break;
         }
     }
     
@@ -41,7 +44,9 @@ class Controller {
         include 'view/telaLogoutView.php';
     }
     
-    
+    private function showTelaLuiza(){
+        include 'view/telaLoginView.php';
+    }
 }
 
 ?>
